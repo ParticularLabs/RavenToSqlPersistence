bd52e99ae5e32ee0b5a2831cc7607303be2950de
David Boike <david.boike@gmail.com>
2017-05-11 13:21:53 -0500

    Program structure, initial commit

---
+++ RavenToSqlPersistence/Configuration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Persistence.Sql;
+using Raven.Client.Document;
+
+namespace RavenToSqlPersistence
+{
+    static class Configuration
+    {
+        public static DocumentStore RavenDB(string endpointName)
+        {
+            var docStore = new DocumentStore()
+            {
+                Url = "http://localhost:8084",
+                DefaultDatabase = "RavenToSqlPersistence",
+
+            };
+
+            // Do not Initialize DocumentStore
+            return docStore;
+        }
+
+        public static void SqlPersistence(EndpointConfiguration endpointConfiguration)
+        {
+            var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+            var connectionStr = @"Data Source=.\SQLEXPRESS;Initial Catalog=RavenToSqlPersistence;Integrated Security=True";
+            persistence.SqlVariant(SqlVariant.MsSqlServer);
+            persistence.ConnectionBuilder(
+                connectionBuilder: () =>
+                {
+                    return new SqlConnection(connectionStr);
+                });
+            persistence.SubscriptionSettings().DisableCache();
+        }
+    }
+}
--- RavenToSqlPersistence/Program.cs
+++ RavenToSqlPersistence/Program.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Configuration.AdvanceExtensibility;
+using NServiceBus.Features;
+using NServiceBus.ObjectBuilder;
+using NServiceBus.Persistence.Sql;
+using NServiceBus.Unicast.Subscriptions.MessageDrivenSubscriptions;
+using Raven.Client.Document;
 
 namespace RavenToSqlPersistence
 {
@@ -10,6 +18,78 @@ namespace RavenToSqlPersistence
     {
         static void Main(string[] args)
         {
+            string endpointName = "TestEndoint";
+            try
+            {
+                RunAsync(endpointName).GetAwaiter().GetResult();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("Conversion tool complete. Press Enter to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        static async Task RunAsync(string endpointName)
+        {
+            var endpointConfiguration = new EndpointConfiguration(endpointName);
+            endpointConfiguration.UseTransport<MsmqTransport>();
+            endpointConfiguration.SendOnly();
+            endpointConfiguration.SendFailedMessagesTo("error");
+
+            //var recoverability = endpointConfiguration.Recoverability();
+            //recoverability.Immediate(customize => customize.NumberOfRetries(0));
+            //recoverability.Delayed(customize => customize.NumberOfRetries(0));
+
+            Configuration.SqlPersistence(endpointConfiguration);
+
+            var docStore = Configuration.RavenDB(endpointName);
+            docStore.Initialize();
+
+            var endpoint = await Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false);
+
+            var builder = GetBuilderFeature.Builder;
+
+            await new SubscriptionConverter(docStore, builder).Run();
+        }
+    }
+
+    public class GetBuilderFeature : Feature
+    {
+        public static IBuilder Builder { get; private set; }
+        public GetBuilderFeature()
+        {
+            EnableByDefault();
+        }
+
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+            context.RegisterStartupTask(builder => new Startup(builder));
+        }
+
+        class Startup : FeatureStartupTask
+        {
+            private IBuilder builder;
+
+            public Startup(IBuilder builder)
+            {
+                GetBuilderFeature.Builder = builder;
+            }
+
+            protected override Task OnStart(IMessageSession session)
+            {
+                return Task.CompletedTask;
+            }
+
+            protected override Task OnStop(IMessageSession session)
+            {
+                return Task.CompletedTask;
+            }
         }
     }
 }

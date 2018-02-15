9e3311822f91152f7c9e99586efa31edebd0282c
Mauro Servienti <mauro.servienti@gmail.com>
2018-02-15 15:09:31 +0100

    Make sure all timeouts are loaded and converted

--- RavenToSqlPersistence/Timeouts/TimeoutConverter.cs
+++ RavenToSqlPersistence/Timeouts/TimeoutConverter.cs
@@ -2,23 +2,16 @@
 using NServiceBus.Extensibility;
 using Raven.Client.Document;
 using NServiceBus.TimeoutPersisters.RavenDB;
+using RavenToSqlPersistence.Utility;
 
 static class TimeoutConverter
 {
     public static async Task ConvertTimeouts(DocumentStore docStore)
     {
-        using (var session = docStore.OpenAsyncSession())
+        foreach (var timeout in docStore.AllDocumentsStartingWith<TimeoutData>(Configuration.TimeoutDocumentsStartWith, 1024))
         {
-            var batch = await session.Advanced.LoadStartingWithAsync<TimeoutData>(
-                    keyPrefix: Configuration.TimeoutDocumentsStartWith, 
-                    start: 0, 
-                    pageSize: 1024);
-
-            foreach (var timeout in batch)
-            {
-                var proxy = EndpointProxy.GetProxy(timeout.OwningTimeoutManager);
-                await proxy.TimeoutStorage.Add(timeout.ToCoreTimeoutData(), new ContextBag());
-            }
+            var proxy = EndpointProxy.GetProxy(timeout.OwningTimeoutManager);
+            await proxy.TimeoutStorage.Add(timeout.ToCoreTimeoutData(), new ContextBag());
         }
     }
 }

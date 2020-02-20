0d823145f21799ffcdeeafed70d334f4368df628
Ramon Smits <ramon.smits@gmail.com>
2020-02-20 14:59:16 +0100

    Added logging of expired messages.

--- RavenToSqlPersistence/Timeouts/TimeoutConverter.cs
+++ RavenToSqlPersistence/Timeouts/TimeoutConverter.cs
@@ -21,7 +21,10 @@ static class TimeoutConverter
         var t = senderConfig.UseTransport<RabbitMQTransport>();
         t.ConnectionString("host=localhost");
         t.UseConventionalRoutingTopology();
-        var sender = await RawEndpoint.Start(senderConfig);
+        var sender = await RawEndpoint.Start(senderConfig)
+            .ConfigureAwait(false);
+
+        var now = DateTime.UtcNow;
 
         // Based on https://github.com/Particular/NServiceBus/blob/ead779f33e8bdec6844ee99a892729cfd7b7f0bc/src/NServiceBus.Core/DelayedDelivery/TimeoutManager/StoreTimeoutBehavior.cs#L63-L65
         foreach (var timeout in docStore.AllDocumentsStartingWith<TimeoutData>(Configuration.TimeoutDocumentsStartWith, 1024))
@@ -32,6 +35,7 @@ static class TimeoutConverter
             var headers = timeout.Headers;
             var timestamp = timeout.Time;
             var destination = timeout.Destination;
+            var id = headers["NServiceBus.MessageId"];
 
             if (ignoreMachineName)
             {
@@ -42,6 +46,8 @@ static class TimeoutConverter
                     destination = destination.Substring(0, at);
                 }
             }
+
+            var age = now - timestamp;
 
             var request = new OutgoingMessage(
                 messageId: timeout.Id,
@@ -54,8 +60,16 @@ static class TimeoutConverter
                 request,
                 new UnicastAddressTag(destination)
                 );
-            
-            operation.DeliveryConstraints.Add(new DoNotDeliverBefore(timestamp));
+
+            if (age.Ticks>0)
+            {
+                await Console.Out.WriteLineAsync($"Warning: Message {id} was scheduled for {timestamp} which passed {age} ago.")
+                    .ConfigureAwait(false);
+            }
+            else
+            {
+                operation.DeliveryConstraints.Add(new DoNotDeliverBefore(timestamp));
+            }
 
             await sender.Dispatch(
                 outgoingMessages: new TransportOperations(operation),
